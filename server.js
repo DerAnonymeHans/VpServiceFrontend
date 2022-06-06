c6543e98bb779983a6f71d9762dfb887e6df3c9c
Pascal <pascal.setzer@gmx.de>
2022-06-06 20:47:42 +0200

    removed env port because not allowed outside module

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use("/", serveStatic(path.join(__dirname, "/dist")))
 
-const port = import.meta.env.VITE_PORT || 8080;
+const port = 8080;
 app.listen(port);
 
 console.log("Starting on port " + port);
