8361dbcf2d23730b4f8f7fd647a8c5b7d867f286
Pascal <pascal.setzer@gmx.de>
2022-06-27 09:46:22 +0200

    v1.4.2- https test

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ import Footer from "@/components/footer/Footer.vue";
 export default {};
 const URL = import.meta.env.VITE_API_URL;
 console.log(URL);
+
 const fetchAPI = (path, obj = {}) => fetch(URL + path, obj);
 
 const sleep = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms));
