38057f9b365da808fa7cce0b68cdfe4fb1787448
Pascal <pascal.setzer@gmx.de>
2022-08-05 15:05:39 +0200

    v0.23 - new manifest, included workers again

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { Vue3Mq } from "vue3-mq";
-// import "./registerServiceWorker";
+import "./registerServiceWorker";
 
 const app = createApp(App);
 
